afb8a15363b863eb56d372878aa7310023d26fe4
nmjmdr <narasimha.gm@gmail.com>
2017-09-10 23:11:19 +1000

    moved main.go to src

---
+++ src/main.go
@@ -0,0 +1,49 @@
+package main
+
+import "github.com/gorilla/mux"
+import "net/http"
+import "log"
+import "fmt"
+import "filter"
+import "queryfunctions"
+import "theshows"
+import "encoding/json"
+
+var itemsFilter filter.ItemsFilter
+
+type ErrorResponse struct {
+  Error string
+}
+
+func RootHandler(w http.ResponseWriter, r *http.Request) {
+
+  w.Header().Set("content-type", "application/json")
+
+  var request theshows.Request
+  err := json.NewDecoder(r.Body).Decode(&request)
+  if err != nil {
+    json.NewEncoder(w).Encode(ErrorResponse{ Error: "Could not decode request: JSON parsing failed"})
+    return
+  }
+  var results []theshows.Result
+  var show theshows.Show
+
+	itemsFilter := filter.NewItemsFilter(&show)
+
+  fieldQuery1 := queryfunctions.IsTrue("drm")
+  fieldQuery2 := queryfunctions.IsGreaterThanN("episodeCount", 3)
+
+  for _, item := range request.Payload {
+    if itemsFilter.IsMatch(item, []filter.FieldQuery{fieldQuery1, fieldQuery2}) {
+      results = append(results, theshows.ToResult(item))
+    }
+  }
+  fmt.Println(results)
+  json.NewEncoder(w).Encode(results)
+}
+
+func main() {
+  router := mux.NewRouter()
+  router.HandleFunc("/", RootHandler).Methods("POST")
+  log.Fatal(http.ListenAndServe(":9090", router))
+}
